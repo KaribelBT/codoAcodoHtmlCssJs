5617ca055e1939ffa8234371c6adedfaa1f77441
karibelbt <karibel.barco@gmail.com>
2022-05-18 17:37:17 -0300

    validate form

---
+++ js/validate-form.js
@@ -0,0 +1,107 @@
+const form = document.getElementById('form');
+const inputs = document.querySelectorAll('#form input');
+const tphone = document.querySelectorAll('input[name="tphone"]');
+const reason = document.getElementById('reason');
+const message = document.getElementById('message');
+
+const expresions = {
+	fname: /^[a-zA-ZÀ-ÿ\s]{2,40}$/,
+    lname: /^[a-zA-ZÀ-ÿ\s]{2,40}$/,
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    phone: /^\d{7,14}$/,    
+    subject: /^.{1,100}$/,
+    message: /^.{1,350}$/
+}
+
+const fields = {
+	fname: false,
+	lname: false,
+	email: false,
+	tphone: false,
+	phone: false,
+    reason: true,
+    subject: false,
+    message: false
+}
+
+const validateForm = (e) => {
+	switch (e.target.name) {
+		case 'fname':
+			validateField(expresions.fname, e.target, 'fname');
+		break;
+        case 'lname':
+			validateField(expresions.lname, e.target, 'lname');
+		break;
+		case 'email':
+			validateField(expresions.email, e.target, 'email');
+		break;
+        case 'tphone':
+			validateOptions(e.target, 'tphone');
+		break;
+		case 'phone':
+			validateField(expresions.phone, e.target, 'phone');
+		break;
+        case 'reason':
+			validateOptions(e.target, 'reason');
+		break;
+        case 'subject':
+			validateField(expresions.subject, e.target, 'subject');
+		break;
+        case 'message':
+			validateField(expresions.message, e.target, 'message');
+		break;
+	}
+}
+
+const validateField = (expresion, input, field) => {
+	if(expresion.test(input.value)){
+        document.querySelector(`#${field}-group .form-field-error`).classList.remove('form-field-error-active');
+        document.querySelector(`#${field}-group`).classList.remove('input-container-extra-margin');
+        document.querySelector(`#${field}-group i`).classList.add('fa-check-circle');
+        document.querySelector(`#${field}-group i`).classList.add('form-icon-ok');
+        document.querySelector(`#${field}-group i`).classList.remove('fa-times-circle');
+        document.querySelector(`#${field}-group i`).classList.remove('form-icon-error');
+		fields[field] = true;
+	} else {
+        document.querySelector(`#${field}-group .form-field-error`).classList.add('form-field-error-active');
+        document.querySelector(`#${field}-group`).classList.add('input-container-extra-margin');
+        document.querySelector(`#${field}-group i`).classList.add('fa-times-circle');
+        document.querySelector(`#${field}-group i`).classList.add('form-icon-error');
+        document.querySelector(`#${field}-group i`).classList.remove('fa-check-circle');
+        document.querySelector(`#${field}-group i`).classList.remove('form-icon-ok');
+		fields[field] = false;
+	}
+}
+
+const validateOptions = (input, field) => {
+    document.querySelector(`#${field}-group .form-field-error`).classList.remove('form-field-error-active');
+    document.querySelector(`#${field}-group`).classList.remove('input-container-extra-margin');
+    fields[field] = true
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validateForm);
+	input.addEventListener('blur', validateForm);
+});
+message.addEventListener('keyup', validateForm);
+message.addEventListener('blur', validateForm);
+tphone.forEach((input) => {
+	input.addEventListener('change', validateForm);
+});
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+    if(!document.querySelectorAll('input[name="tphone"]:checked').length) {
+        document.querySelector(`#tphone-group .form-field-error`).classList.add('form-field-error-active');
+    }
+    if(reason.options[reason.selectedIndex].value === "none") {
+        document.querySelector(`#reason-group .form-field-error`).classList.add('form-field-error-active');
+    }
+    const isFormValid = Object.values(fields).every(
+        value => value === true
+    );
+    console.log(fields)
+    if(isFormValid) {
+        console.log('exitoood')
+    }
+});
